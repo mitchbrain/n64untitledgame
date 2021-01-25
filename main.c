9bb60a29416e276fd5ba1ffbe6bf505513db22f5
Wesley Strong <wesstr@gmail.com>
2021-01-24 19:27:39 -0600

    Tinkering with transforms

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <malloc.h>
 #include <string.h>
 #include <stdint.h>
+#include <math.h>
 #include <libdragon.h>
 
 //#include <ControllerInput.h>
@@ -17,6 +18,42 @@ struct controller_data gKeys;
 
 volatile int gTicks;                    /* incremented every vblank */
 
+typedef struct SpriteTransform SpriteTransform;
+
+struct SpriteTransform 
+{
+    // XY position
+    float X;
+    float Y;
+
+    // Rotation position
+    float rX;
+    float rY;
+
+    // Scale
+    float sX;
+    float sY;
+
+    sprite_t * obj;
+    display_context_t* _dc;
+};
+
+void translateSprite(SpriteTransform* object, SpriteTransform amount)
+{
+    // Move object to new location
+    object->X = ((object->X * cos(amount.rX)) - (object->Y * sin(amount.rX))) + amount.X; 
+    object->Y = ((object->X * sin(amount.rY)) + (object->Y * cos(amount.rY))) - amount.Y;
+
+    // Rotate object
+    //object->X = (object->X * cos(amount.rX)) - (object->Y * sin(amount.rX));
+    //object->Y = (object->X * sin(amount.rY)) + (object->Y * cos(amount.rY));
+
+    // Render at new cordinates
+    graphics_draw_sprite_trans(*object->_dc, object->X, object->Y, object->obj );
+    //rdp_draw_sprite_scaled();
+    rsp_init();
+}
+
 /* input - do getButtons() first, then getAnalogX() and/or getAnalogY() */
 unsigned short getButtons(int pad)
 {
@@ -28,7 +65,6 @@ unsigned short getButtons(int pad)
 
 float getAnalogX(int pad)
 {
-
     return (float)gKeys.c[pad].x/114;
 }
 
@@ -103,9 +139,16 @@ void printDebug(display_context_t dc, char* msg, int x, int y)
 	printText(dc, temp, x, y);
 }
 
+
 /* main code entry point */
 int main(void)
 {
+    display_context_t _dc;
+	//int box_x = 20, box_y = 20; 
+
+#ifdef DEBUG
+	char temp[128];
+#endif
     /* enable interrupts (on the CPU) */
     init_interrupts();
 
@@ -116,6 +159,10 @@ int main(void)
     controller_init();
     timer_init();
 
+    SpriteTransform movment = {0.0, 0.0,
+                               0.0, 0.0,
+                               0.0, 0.0 };
+
     /* Read in single sprite */
 
     int fp = dfs_open("/face.sprite");
@@ -123,33 +170,49 @@ int main(void)
     dfs_read( face, 1, dfs_size( fp ), fp );
     dfs_close( fp );
 
-    #ifdef DEBUG
-        char temp[128];
-    #endif
-    
-    int box_x = 20, box_y = 20;
+    SpriteTransform faceTrasform = {0.0, 0.0,
+                                    0.0, 0.0,
+                                    0.0, 0.0,
+                                    face };
+    faceTrasform._dc = &_dc;
+
+    float y_input[4] = {0,0,0,0};
+    float x_input[4] = {0,0,0,0};	
+
+    //const float gravity_const = 1;
 
     while(1) 
     {
+        _dc = lockVideo(1);
+		graphics_fill_screen(_dc, graphics_make_color(0,0,0,255));
 
+		getButtons(0);
 
-        static display_context_t _dc = 0;
+		movment.Y = getAnalogY(0);
+		movment.X = getAnalogX(0);
 
-        /* Grab a render buffer */
-        while( !(_dc = display_lock()) );
-       
-        /*Fill the screen */
-        graphics_fill_screen( _dc, 0xFFFFFFFF );
+        if (gKeys.c[0].A)
+        {
+            movment.rX = 1;
+        }
 
-        /* Set the text output color */
-        graphics_set_color( 0x0, 0xFFFFFFFF );
+        if (gKeys.c[0].B)
+        {
+            movment.rY = 1;
+        }
 
-        controller_scan();        
-        getButtons(0);
+        translateSprite(&faceTrasform, movment);
 
-		float y_input[4] = {getAnalogY(0), getAnalogY(1), getAnalogY(2), getAnalogY(3)};
-		float x_input[4] = {getAnalogX(0), getAnalogX(1), getAnalogX(2), getAnalogX(3)};	
+		y_input[1] = getAnalogY(1);
+		x_input[1] = getAnalogX(1);	
 
+		y_input[2] = getAnalogY(2);
+		x_input[2] = getAnalogX(2);	
+
+		y_input[3] = getAnalogY(3);
+		x_input[3] = getAnalogX(3);	
+
+        graphics_draw_line(_dc,0, (240/2), 340, (240/2), graphics_make_color(255, 0, 0, 255));
 
 #ifdef DEBUG
 		sprintf(temp, "Y0: %f, X0: %f", y_input[0], x_input[0]);
@@ -169,16 +232,29 @@ int main(void)
 		memset(temp, 0, sizeof(temp));
 #endif
 
-		if (y_input != 0)
-		{
-			box_y += -y_input[0] * SENSITIVITY;
-		}
+      //  for (int i = 0; i !=1; i++)
+      //  {
+      //      if (y_input[i] != 0)
+      //      {
+      //          box_y += -y_input[i] * SENSITIVITY;
+      //      }
 
-		if (x_input != 0)
-		{
-			box_x += x_input[0] * SENSITIVITY;
-		}
-        graphics_draw_sprite_trans( _dc, box_x, box_y, face );
+      //      if (x_input != 0)
+      //      {
+      //          box_x += x_input[i] * SENSITIVITY;
+      //      }
+      //  }
+        
+        // If we have not hit the ground then apply velocity
+      //  if (box_y <= (120 - (face->height)))
+      //  {
+      //      box_y += gravity_const;
+      //  }
+
+
+        unlockVideo(_dc);
+
+        //graphics_draw_sprite_trans( _dc, box_x, box_y, face );
 
         /* Force backbuffer flip */
         display_show(_dc);
